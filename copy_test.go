5af1a4742e6bf4c3fdebfca74f8a509da73b9594
kolaworld <kola@hey.com>
2025-02-16 21:08:54 -0500

    fix imports

--- copy_test.go
+++ copy_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	fieldmask_utils "github.com/mennanov/fieldmask-utils"
+	fieldmask_utils "github.com/shftcmd/fieldmask-utils"
 )
 
 func TestStructToStruct_SimpleStruct(t *testing.T) {
--- mask_test.go
+++ mask_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/genproto/protobuf/field_mask"
 
-	fieldmask_utils "github.com/mennanov/fieldmask-utils"
+	"github.com/shftcmd/fieldmask-utils"
 )
 
 func TestMask_String(t *testing.T) {
