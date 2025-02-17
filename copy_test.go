d585e13976a3190c0dd67501b04c6cf90dad0dd2
kolaworld <kola@hey.com>
2025-02-16 20:48:24 -0500

    no error on empty subfilter

--- copy_test.go
+++ copy_test.go
@@ -1013,7 +1013,10 @@ func TestStructToStruct_SameInterfacesNonPtr_NonEmptyDst(t *testing.T) {
 
 	mask := fieldmask_utils.MaskFromString("Stringer")
 	err := fieldmask_utils.StructToStruct(mask, src, dst)
-	assert.Error(t, err)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Jessica", src.Stringer.String())
+	assert.Equal(t, "Adam", dst.Stringer.String())
 }
 
 func TestStructToStruct_NonPtrDst(t *testing.T) {
